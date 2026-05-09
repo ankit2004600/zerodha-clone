bf81ac7b94076e3f8fafc8e1a4ab93ee6776016b
Ankit Kumar <ankitkumar11924@gmail.com>
2026-05-09 18:40:03 +0530

    final successful build

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -3,7 +3,7 @@ function Hero() {
     return (  
      <div className ="container p-5">
          <div className ="row text-center">
-            <img src = "media/images/homeHero.png" alt = "Hero Image" className ="mb-5"/>
+            <img src = "media/images/homeHero.png" alt="Home Hero" className ="mb-5"/>
             <h1 className = "mt-5">Invest in everything</h1>
             <p>Online platform to invest in stocks, derivatives,mutual funds</p>
             <button  className="p-2 btn btn-primary fs-5" style ={{width:"20%",margin:"0 auto"}}>Signup Now</button>
